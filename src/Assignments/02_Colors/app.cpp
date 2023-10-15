4e23ba9564c469e6c5a221212581b761625b14a4
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-10-15 18:49:47 +0200

    Colors 2

--- src/Assignments/02_Colors/app.cpp
+++ src/Assignments/02_Colors/app.cpp
@@ -35,15 +35,15 @@ void SimpleShapeApplication::init() {
 
     // A vector containing the x,y,z vertex coordinates for the triangle.
     std::vector<GLfloat> vertices = {
-            -0.5f, 0.0f, 0.0f,
-            0.5f, 0.0f, 0.0f,
-            0.0f, 0.5f, 0.0f,
-            -0.5f, 0.0f, 0.0f,
-            0.5f, 0.0f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            0.5f, 0.0f, 0.0f,
-            0.5f, -0.5f, 0.0f
+            -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
     };
 
     /*
