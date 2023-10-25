e258e5d1358a5389570dedf351e597003c088c48
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-10-25 22:18:02 +0200

    Uniforms 1 - MovingHouses

--- src/Assignments/04_Uniforms/shaders/base_vs.glsl
+++ src/Assignments/04_Uniforms/shaders/base_vs.glsl
@@ -3,6 +3,12 @@
 layout(location=0) in vec4 a_vertex_position;
 layout(location=1) in vec4 a_vertex_color;
 
+layout(std140) uniform Transformations {
+    vec2 scale;
+    vec2 translation;
+    mat2 rotation;
+};
+
 out vec4 vertex_color;
 
 void main() {
